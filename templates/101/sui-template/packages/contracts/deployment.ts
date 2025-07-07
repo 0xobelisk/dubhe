77d4137fe95a05be560e2eed409547289e414cab
vladilen11 <vladilenaksana@163.com>
2025-07-07 16:43:29 +0800

    feat: update node,sui packages version

--- templates/101/sui-template/packages/contracts/deployment.ts
+++ templates/101/sui-template/packages/contracts/deployment.ts
@@ -1,5 +1,5 @@
 type NetworkType = 'testnet' | 'mainnet' | 'devnet' | 'localnet';
 
 export const NETWORK: NetworkType = 'localnet';
-export const PACKAGE_ID = '0xad3da7b0681b16830878dd908fb30418da7d86871c445866174aee28e45850d6';
-export const DUBHE_SCHEMA_ID = '0x38ba44b646ac909ddcc0078f1a9f396f32cd9ce0156c6ea2fd9c33c1046ea4f5';
+export const PACKAGE_ID = '0xb4aa432e16b9d582d6aa3a2be33c9c50decd7fae70aa94e36b2f275ac61de568';
+export const DUBHE_SCHEMA_ID = '0x2ccdb4b68482909a2b998637b187194c9f4d0a0ddd1d398fbbcf24a28236350c';
