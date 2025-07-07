6fff625bdd51aa24169e75c3fcfa1cd5504a54c4
vladilen11 <vladilenaksana@163.com>
2025-07-07 16:46:28 +0800

    feat: update .gitignore

--- templates/101/sui-template/packages/contracts/deployment.ts
+++ templates/101/sui-template/packages/contracts/deployment.ts
@@ -1,5 +1,5 @@
 type NetworkType = 'testnet' | 'mainnet' | 'devnet' | 'localnet';
 
 export const NETWORK: NetworkType = 'localnet';
-export const PACKAGE_ID = '0xb4aa432e16b9d582d6aa3a2be33c9c50decd7fae70aa94e36b2f275ac61de568';
-export const DUBHE_SCHEMA_ID = '0x2ccdb4b68482909a2b998637b187194c9f4d0a0ddd1d398fbbcf24a28236350c';
+export const PACKAGE_ID = '0xc151eaccdb11372cb19fbd087b9e61b9047e8f0f83272ccb4f30c6bc8395e49f';
+export const DUBHE_SCHEMA_ID = '0x5c8d7dfed2e59bd0e744ff3a23fdee059c5c86c097e62de4a0b41b4afe6cdf99';
