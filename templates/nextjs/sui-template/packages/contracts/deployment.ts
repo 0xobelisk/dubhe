abe80aa15045dffaca298a1b31505d7df217a5b8
vladilen11 <vladilenaksana@163.com>
2025-09-11 02:43:15 +0800

    feat(sui-template): update nextjs useContract to useDubhe

--- templates/nextjs/sui-template/packages/contracts/deployment.ts
+++ templates/nextjs/sui-template/packages/contracts/deployment.ts
@@ -1,5 +1,5 @@
 type NetworkType = 'testnet' | 'mainnet' | 'devnet' | 'localnet';
 
 export const NETWORK: NetworkType = 'localnet';
-export const PACKAGE_ID = '0x5351cfed3b3071a8ad24c84863f84b803e03ac010afa832d3ad85358cc471554';
-export const DUBHE_SCHEMA_ID = '0xbc9d3092433f2bd8a8f7094a44a06ad359ff1bf011492e09870e766695f379b0';
+export const PACKAGE_ID = '0x37152de0304d99183e57847e79ba488ad63fe1504ca7d80e7e6a3cf3f43465af';
+export const DUBHE_SCHEMA_ID = '0x6639bbe1c041f4b8a363646486f3005482cf6efe64fd6e88bcc34ba40abc196d';
