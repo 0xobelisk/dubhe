52cc932cb11ec8d8d0a8eab27949926374756e07
vladilen11 <vladilenaksana@163.com>
2026-03-18 22:34:22 +0800

    feat(docs): update schema docs, add framework docs

--- docs/pages/dubhe/sui/schemas/_meta.ts
+++ docs/pages/dubhe/sui/schemas/_meta.ts
@@ -1,6 +1,7 @@
 export default {
   installation: 'Installation',
   config: 'Schemas Config',
+  framework: 'Framework Architecture',
   'reading-and-writing': 'Reading & Writing',
   'support-type': 'Supported Types'
 };
